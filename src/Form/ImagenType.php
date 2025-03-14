404d07d5b3611ed2f15bd847e98ba658b09d8c73
Facundo <facundogarnica1996@gmail.com>
2025-03-13 23:20:18 -0300

    Crud Creados

---
+++ src/Form/ImagenType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Imagen;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImagenType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Tipo')
+            ->add('Url')
+            ->add('Post')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Imagen::class,
+        ]);
+    }
+}
